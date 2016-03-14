8fb2eaa148371d2fce8b63b47f9ac1d910992281
Guilherme Sa <guilsa001@gmail.com>
2016-03-14 15:50:49 -0700

    Serve public files from Heroku

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-})
+app.use(express.static(path.join(__dirname, './public')));
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// })
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
