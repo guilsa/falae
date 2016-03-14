0afefbe8e1ea8e55db933cec1f231995b785cfc3
Guilherme Sa <guilsa001@gmail.com>
2016-03-14 16:05:44 -0700

    Restructured app for Heroku, add port, ejs templating

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, './public')));
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// })
+// set the home page route
+app.get('/', function(req, res) {
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
@@ -16,6 +25,10 @@ io.on('connection', function(socket){
   });
 });
 
+app.listen(port, function() {
+    console.log('app.listen - Our app is running on http://localhost:' + port);
+});
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('http - listening on *:3000');
 })
